7dacdb12591a08f083ec6e518853674f9203fdd0
Lorenzo Mariani <56412006+lorenzo-mariani@users.noreply.github.com>
2021-11-20 15:50:24 +0100

    Create insert.sql

---
+++ usermanager/sql/insert.sql
@@ -0,0 +1,8 @@
+insert into users values ('mario.rossi@email.com', 'ciao', 'Mario','Rossi', 'Ferrero International S.A.');
+insert into users values ('luigi.verdi@email.com', 'ciao', 'Luigi','Verdi', 'Ferrero International S.A.');
+
+insert into users values ('giovanni.bianchi@email.com', 'ciao', 'Giovanni','Bianchi', 'The Coca-Cola Company');
+insert into users values ('federico.azzurri@email.com', 'ciao', 'Federico','Azzurri', 'The Coca-Cola Company');
+
+insert into aziende values ('Ferrero International S.A.','20000000001');
+insert into aziende values ('The Coca-Cola Company','20000000002');
